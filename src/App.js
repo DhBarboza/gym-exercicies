5a2b749bc7f5f987719f13eff91139218ae7ea7c
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-02-27 22:25:53 -0300

    Feature: Progress NAvbar development

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
 import { ExerciseDetails } from "./pages/ExerciseDetails";
+import { Footer } from "./components/Footer";
 
 export const App = () => {
     return (
-        <Box>
+        <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
             <Navbar />
             <Routes>
-                <Route path="./" element={<Home />} />
-                <Route path="./exercise/:id" element={<ExerciseDetails />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/exercise/:id" element={<ExerciseDetails />} />
             </Routes>
+            <Footer />
         </Box>
     );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+
+import Logo from "../assets/images/Logo.png"
 
 export const Navbar = () => {
     return <div>Navbar</div>;
